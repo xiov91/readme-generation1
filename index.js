288cb64008dc9621fad530c60f0286cb18ce2ee2
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-06-25 17:30:54 -0400

    questions start

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'motivation',
+        message: "What was your motivation behind this project?",
+    },
+    {
+        type: 'input',
+        name: 'problem',
+        message: "What problem does it solve?"
+    },
+    {
+        type: 'input',
+        name: 'lesson',
+        message: "What did you learn?"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
