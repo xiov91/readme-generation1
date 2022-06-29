9e640d8779abdb5d16cfce0bd0b15411024505bc
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-06-29 17:57:09 -0400

    added questions and template

--- index.js
+++ index.js
@@ -3,26 +3,126 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'motivation',
-        message: "What was your motivation behind this project?",
-    },
-    {
-        type: 'input',
-        name: 'problem',
-        message: "What problem does it solve?"
-    },
-    {
-        type: 'input',
-        name: 'lesson',
-        message: "What did you learn?"
-    }
-];
+/* const questions = [
+     // type: input to add your own text,
+    // message: the question posed,
+    // name: like a class or id almost,
+    //validate: used to make sure something is input when it's necessary
+
+    // OR
+
+    // type: checkbox to pick multiple options,
+    // message: the question posed,
+    // name: like a class or id almost,
+    // choices: ['has', 'to', 'be', 'an', 'array'] 
+
+]*/
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+            validate: (value)=>{ if(value){return true} else {return "This answer is required!"}}
+        },
+        {
+            type: 'input',
+            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+            name: 'installation',
+            validate: (value)=>{ if(value){return true} else {return "This answer is required!"}}
+        },
+        {
+            type: 'input',
+            message: 'Provide instructions and examples for use.',
+            name: 'usage',
+            validate: (value)=>{ if(value){return true} else {return "This answer is required!"}}
+        },
+        {
+            type: 'input',
+            message: 'List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well. ',
+            name: 'Credits',
+            validate: (value)=>{ if(value){return true} else {return "This answer is required!"}}
+        },
+        {
+            type: 'input',
+            message: 'Please input your license type.',
+            name: 'license',
+            validate: (value)=>{ if(value){return true} else {return "This answer is required!"}}
+        },
+        {
+            type: 'input',
+            message: 'Include guidelines for how other users can contribute!',
+            name: 'contribution',
+            validate: (value)=>{ if(value){return true} else {return "This answer is required!"}}
+        },
+        {
+            type: 'input',
+            message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them here.',
+            name: 'tests',
+            validate: (value)=>{ if(value){return true} else {return "This answer is required!"}}
+        }
+
+
+
+    ])
+// .then(({
+    // all the questions
+//}) => {
+/* 
+const template = `# {title}
+
+## Description
+${description}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Contributions](#contributions)
+* [Tests](#tests)
+* [Questions](#questions)
+
+# Installation
+${installation}
+
+## Usage
+${usage}
+
+## Credits
+${credits}
+
+## License
+${license}
+
+## Contributions
+${contributions}
+
+## Tests
+${tests}
+
+## Questions
+${questions}`
+
+}
+) */
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+// function to create our readme using fs
+// createNewFile(title, template);
+
+//creating our createnewfile function
+//function createnewfile(filename, data)
+//fs
+//fs.writeFile('./${fileName.toLowerCase().split(' ').join('')}.md',data, ()=>{
+    // if(err){
+        //console.log(err);
+    //}
+    //console.log('README complete!');
+//}
 
 // TODO: Create a function to initialize app
 function init() {}
